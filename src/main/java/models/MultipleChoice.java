6e4382af4c0f1c3670594ac3c1a06d9f2c28aade
ggulo <ggulo22@freeuni.edu.ge>
2025-06-25 21:56:00 +0400

    Added MultipleChoice class which was not seen for some reason

--- src/main/java/models/MultipleChoice.java
+++ src/main/java/models/MultipleChoice.java
@@ -4,13 +4,12 @@
     import java.util.List;
 
     public class MultipleChoice extends Questions{
-    private final ArrayList<Option> options;
-
+    private final ArrayList<Option> optionsList;
 
     public MultipleChoice(int id, String questionText,ArrayList<Option> options) {
         super(id, questionText);
-        this.options = options;
+        this.optionsList = options;
     }
 
-    public ArrayList<Option> getOptions() { return options; }
+    public ArrayList<Option> getOptions() { return optionsList; }
 }
